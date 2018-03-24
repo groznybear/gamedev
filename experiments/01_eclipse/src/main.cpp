e7ef17a12c7a2dbd67c956a27934f09fa7cfce63
groznybear <groznybear@gmail.com>
2018-03-24 20:28:46 +0300

    Initial source code

---
+++ experiments/01_eclipse/src/main.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : 01_eclipse.cpp
+// Author      : Daniil Cherepkov
+// Version     :
+// Copyright   :
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello, World!" << endl; // prints Hello, World!
+    return 0;
+}
