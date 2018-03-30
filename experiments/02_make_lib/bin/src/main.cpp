8c7cc8ace7e64ff236dd092216a98226071fde9d
groznybear <groznybear@gmail.com>
2018-03-30 23:33:54 +0300

    Building with/without lib

--- experiments/02_make_lib/bin/src/main.cpp
+++ experiments/02_make_lib/bin/src/main.cpp
@@ -4,12 +4,16 @@
  *  Created on: 25 мар. 2018 г.
  *      Author: groznybear
  */
+#include "mainconfig.hpp"
 
+#ifdef HELLOLIB
+#include "lib.hpp"
+#endif
 #include <iostream>
-#include <lib.hpp>
 
 int main(int argc, char* argv[])
 {
+    std::cout << "VERSION: " << use_lib_VERSION_MAJOR << "." << use_lib_VERSION_MINOR << std::endl;
     // default world to use to greeting the user, if name wasn't specified
     std::string username = "stranger";
 
@@ -19,5 +23,16 @@ int main(int argc, char* argv[])
         username = argv[1];
     }
 
-    return greetings(username) ? EXIT_SUCCESS : EXIT_FAILURE;
+#ifdef HELLOLIB
+
+    std::cout << "Lib was compiled, using Lib" << std::endl;
+    int exitCode = greetings(username) ? EXIT_SUCCESS : EXIT_FAILURE;
+#else
+    std::cout << "Lib was not compiled, using default" << std::endl;
+    std::cout << "Hello, " << username << std::endl;
+    int exitCode = std::cout.good() ? EXIT_SUCCESS : EXIT_FAILURE;
+#endif
+
+    std::cout << "Program exit with code: " << exitCode << std::endl;
+    return exitCode;
 }
