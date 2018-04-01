91007b894f8fc626557ce2fe5f19f24d28c52a67
groznybear <groznybear@gmail.com>
2018-04-01 07:46:25 +0300

    SDL dynamic linked

--- experiments/02_01_sdl_dynamic/bin/src/main.cpp
+++ experiments/02_01_sdl_dynamic/bin/src/main.cpp
@@ -1,7 +1,25 @@
+#include <SDL2/SDL_version.h>
 #include <iostream>
+
+std::ostream& operator<<(std::ostream& out, const SDL_version& v)
+{
+    out << static_cast<int>(v.major) << '.';
+    out << static_cast<int>(v.minor) << '.';
+    out << static_cast<int>(v.patch);
+    return out;
+}
+
 int main()
 {
-    std::cout << "Empty" << std::endl;
+    SDL_version compiled = { 0, 0, 0 };
+    SDL_version linked = { 0, 0, 0 };
+
+    SDL_VERSION(&compiled);
+    SDL_GetVersion(&linked);
+
+    std::cout << "SDL compiled: " << compiled << std::endl;
+    std::cout << "SDL linked: " << linked << std::endl;
+
     int exitCode = std::cout.good() ? EXIT_SUCCESS : EXIT_FAILURE;
     std::cout << "Program has exited with code: " << exitCode << std::endl;
     return exitCode;
