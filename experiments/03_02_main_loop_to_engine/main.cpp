f4b3040a3283cb098414afa01538cee9c5ee3185
GroznyBear <groznybear@gmail.com>
2018-04-07 21:19:14 +0300

    Main loop to engine class

---
+++ experiments/03_02_main_loop_to_engine/main.cpp
@@ -0,0 +1,49 @@
+//============================================================================
+// Name        : microengine.cpp
+// Author      : Daniil Cherepkov
+// Version     : 1.0
+//============================================================================
+
+#include <algorithm>
+#include <array>
+#include <cstdlib>
+#include <iostream>
+#include <memory>
+#include <string_view>
+
+#include "engine.hpp"
+
+int main(int argc)
+{
+    std::unique_ptr<me::engine, void (*)(me::engine*)> engine(
+        me::create_engine(), me::destroy_engine);
+
+    std::string err = engine->initialize("");
+    if (!err.empty()) {
+        std::cerr << err << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    bool continue_loop = true;
+    while (continue_loop) {
+        me::event event;
+
+        while (engine->read_input(event)) {
+            std::cout << event << std::endl;
+            switch (event) {
+            case me::event::turn_off:
+                continue_loop = false;
+                break;
+            default:
+                break;
+            }
+            if (argc > 1) {
+                std::cout << "CI/CD build. Force escape from main loop." << std::endl;
+            }
+        }
+    }
+
+    engine->uninitialize();
+
+    return EXIT_SUCCESS;
+}
